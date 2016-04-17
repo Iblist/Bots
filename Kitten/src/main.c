9b48da1e288ba3251b4e50b181f68ca572e81b06
Iblist <https://hoskinst@git.socs.uoguelph.ca/cis2500/hoskinst/labs>
2016-04-17 00:20:33 -0400

    Figured out how to get Kitten connecting to an irc channel, need to figure out what to read and write. Something about ping and a pong.

--- Kitten/src/main.c
+++ Kitten/src/main.c
@@ -1,5 +1,11 @@
 #include "mainBot.h"
 
+/*****************
+It should be noted!
+argv[1] should be irc.hackthissite.org
+argv[2] should be 7000
+******************/
+
 void error(char * errorMessage)
 {
     perror(errorMessage);
@@ -8,35 +14,42 @@ void error(char * errorMessage)
 
 int main(int argc, char * argv[])
 {
-    int sockFeed;
-    int server;
+    int sockFeed = SOCK_DGRAM;
+    //sockaddr * serv_addr;
+	struct addrinfo *result;
     //int portNum;
     //int clientSize;
     char bufferIn[255];
-    struct addrinfo hints;
+    //struct addrinfo hints;
 
-    hints.ai_family = AF_UNSPEC;
+    /*hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = 0;
     hints.ai_protocol = 0;   
-    hints.ai_flags = 0; 
+    hints.ai_flags = 0; */
+
+	
 
     if (argc != 3)
     {
-        printf("Correct use is: SERVER_NAME PORT_NUMBER\n");
+        fprintf(stderr, "Correct use is: SERVER_NAME PORT_NUMBER\n");
         exit(1);
     }
 
-    
+    if ((getaddrinfo(argv[1], argv[2], NULL, &result)) != 0)
+	{
+		perror("Error finding server: ");
+		exit(1);
+	}
 
     if ((sockFeed = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("Unable to create Socket.\n");
+        fprintf(stderr, "Unable to create Socket.\n");
         exit(0);
     }
 
-    if (connect(sockFeed, server, sizeof(server)) < 0)
+    if (connect(sockFeed, result->ai_addr, result->ai_addrlen) < 0)
     {
-        printf("Connection Failed.\n");
+        perror("Connection Failed: ");
         exit(0);
     }
     else
@@ -44,7 +57,11 @@ int main(int argc, char * argv[])
         printf("Dude, you did it!  Yay!  Fuck teh police!\n");
     }    
 
-    memset(&bufferIn, 0, sizeof(bufferIn));
+	read(sockFeed, bufferIn, 254);
+
+	printf("bufferIn: %s\n", bufferIn);
+
+    close(sockFeed);
 
     return 0;
 }
